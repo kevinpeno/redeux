6c546a7d58f161d4443b3e5ee1a4720c824eb21d
Kevin Peno <kjpeno@outlook.com>
2021-09-07 18:45:55 -0500

    fix: add vscode/settings back to git

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,1 @@
-export declare function redeux(): void;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,2 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redeux = void 0;
-function redeux() {
-}
-exports.redeux = redeux;
 //# sourceMappingURL=index.js.map
